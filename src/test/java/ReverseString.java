49ab7b882093d6876e854afa11f81c8cd951cbe8
PABRAHP <praveen.a.p@exxonmobil.com>
2020-07-20 19:18:59 +0530

    maven-project

--- src/test/java/ReverseString.java
+++ src/test/java/ReverseString.java
@@ -1,4 +1,15 @@
 public class ReverseString {
 
 
+
+    public static void main(String[] args){
+        String input = "Praveen Abraham";
+        StringBuilder strBuilderInput = new StringBuilder();
+        strBuilderInput.append(input);
+        strBuilderInput.reverse();
+        System.out.println(strBuilderInput);
+
+    }
+
+
 }
